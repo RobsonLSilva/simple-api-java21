3e2810a2130952ffaf1bc35b889a6debd2674661
Robson <robson.lsilva36@gmail.com>
2024-11-19 19:44:06 -0300

    first commit, criacao do projeto, package e arquivo README.md

---
+++ src/main/java/br/com/consumoapi/consumodeapi/ConsumoDeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.consumoapi.consumodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoDeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoDeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/consumoapi/consumodeapi/ConsumoDeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.consumoapi.consumodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoDeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
